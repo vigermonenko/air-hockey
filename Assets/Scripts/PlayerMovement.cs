bd656f2a29ce917a18c19288be8296b6effd044b
Viacheslav Germonenko <v.germonenko@itechart-group.com>
2019-12-30 12:51:17 +0300

    Added score.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,15 @@ public class PlayerMovement : MonoBehaviour
     private bool wasJustClicked = true;
     private bool canMove;
     
-    private Vector2 playerSize;
     private Rigidbody2D rigidBody;
     public Transform Bounder;
-
+    public Collider2D playerCollider;
+    
     Boundary playerBoundary;
 
     void Start () {
-        playerSize = GetComponent<SpriteRenderer>().bounds.extents;
         rigidBody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
         playerBoundary = new Boundary(
             Bounder.GetChild(0).position.y, Bounder.GetChild(1).position.x,
         Bounder.GetChild(2).position.y, Bounder.GetChild(3).position.x
@@ -26,22 +26,13 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetMouseButton(leftMouseButton)) {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
  
-            if (wasJustClicked) {
-                wasJustClicked = false;
- 
-                if ((mousePosition.x >= transform.position.x && mousePosition.x < transform.position.x + playerSize.x ||
-                     mousePosition.x <= transform.position.x && mousePosition.x > transform.position.x - playerSize.x) &&
-                    (mousePosition.y >= transform.position.y && mousePosition.y < transform.position.y + playerSize.y ||
-                     mousePosition.y <= transform.position.y && mousePosition.y > transform.position.y - playerSize.y))
-                {
-                    canMove = true;
-                }
-                else {
-                    canMove = false;
-                }
-            }
-            if (canMove)
+            if (wasJustClicked)
             {
+                wasJustClicked = false;
+
+                canMove = playerCollider.OverlapPoint(mousePosition);
+            }
+            if (canMove) {
                 var clampedX = Mathf.Clamp(mousePosition.x, playerBoundary.Left, playerBoundary.Right);
                 var clampedY = Mathf.Clamp(mousePosition.y, playerBoundary.Down, playerBoundary.Up);
                 var clampedMousePosition = new Vector2(clampedX, clampedY);
---
+++ Assets/Scripts/PuckScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PuckScript : MonoBehaviour
+{
+    private const string AiGoal = "AiGoal";
+    private const string PlayerGoal = "PlayerGoal";
+    
+    public ScoreScript ScoreScriptInstance;
+    public static bool WasGoal { get; private set; }
+    private Rigidbody2D rigidBody;
+    
+    void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+        WasGoal = false;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!WasGoal)
+        {
+            if (other.CompareTag(AiGoal))
+            {
+                ScoreScriptInstance.Increment(ScoreScript.Score.PlayerScore);
+                WasGoal = true;
+                StartCoroutine(ResetPuck());
+            }
+            else if (other.CompareTag(PlayerGoal))
+            {
+                ScoreScriptInstance.Increment(ScoreScript.Score.AiScore);
+                WasGoal = true;
+                StartCoroutine(ResetPuck());
+            }
+        }
+    }
+ 
+    private IEnumerator ResetPuck()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        WasGoal = false;
+        rigidBody.velocity = rigidBody.position = new Vector2(0, 0);
+    }
+}
