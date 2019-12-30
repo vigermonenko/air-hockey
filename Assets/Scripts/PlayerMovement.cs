68b439059ed18fa8a67bdc43df07225232bfc4c0
Viacheslav Germonenko <v.germonenko@itechart-group.com>
2019-12-30 10:59:30 +0300

    Added movement to Red player.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+ 
+public class PlayerMovement : MonoBehaviour {
+ 
+    bool wasJustClicked = true;
+    bool canMove;
+    Vector2 playerSize;
+ 
+ void Start () {
+        playerSize = gameObject.GetComponent<SpriteRenderer>().bounds.extents;
+ }
+ 
+ void Update () {
+ if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+ 
+            if (wasJustClicked)
+            {
+                wasJustClicked = false;
+ 
+                if ((mousePos.x >= transform.position.x && mousePos.x < transform.position.x + playerSize.x ||
+                mousePos.x <= transform.position.x && mousePos.x > transform.position.x - playerSize.x) &&
+                (mousePos.y >= transform.position.y && mousePos.y < transform.position.y + playerSize.y ||
+                mousePos.y <= transform.position.y && mousePos.y > transform.position.y - playerSize.y))
+                {
+                    canMove = true;
+                }
+                else
+                {
+                    canMove = false;
+                }
+            }
+ 
+            if (canMove)
+            {
+                transform.position = mousePos;
+            }
+        }
+        else
+        {
+            wasJustClicked = true;
+        }
+ }
+}
