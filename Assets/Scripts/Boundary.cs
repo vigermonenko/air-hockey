f6b62fb9fa8be817372a794a41a7afaf04802ff7
Viacheslav Germonenko <v.germonenko@itechart-group.com>
2019-12-30 12:19:00 +0300

    Added second player and AI.

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,12 @@
+﻿struct Boundary
+{
+    public float Up, Right, Down, Left;
+
+    public Boundary(float up, float right, float down, float left)
+    {
+        Up = up;
+        Right = right;
+        Down = down;
+        Left = left;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,20 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public Transform Bounder;
 
     Boundary playerBoundary;
-    
-    struct Boundary
-    {
-        public float Up, Right, Down, Left;
 
-        public Boundary(float up, float right, float down, float left)
-        {
-            Up = up;
-            Right = right;
-            Down = down;
-            Left = left;
-        }
-    }
- 
     void Start () {
         playerSize = GetComponent<SpriteRenderer>().bounds.extents;
         rigidBody = GetComponent<Rigidbody2D>();
