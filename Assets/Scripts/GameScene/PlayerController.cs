a8773eef4a6479614b33b0f80f2b1d2e633699ea
Viacheslav Germonenko <vi.germonenko@outlook.com>
2020-01-05 15:37:17 +0300

    Added second player. Built for iOS.

---
+++ Assets/Scripts/GameScene/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   public List<PlayerMovement> Players = new List<PlayerMovement>();
+    
+    void Update()
+    {
+        for (int i = 0; i < Input.touchCount; i++) {
+            Vector2 touchWorldPosition = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+            Players.ForEach(player =>
+            {
+                if (player.LockedFingerId == null) {
+                    if (Input.GetTouch(i).phase == TouchPhase.Began &&
+                        player.PlayerCollider.OverlapPoint(touchWorldPosition)) {
+                        player.LockedFingerId = Input.GetTouch(i).fingerId; 
+                    }
+                }
+                else if (player.LockedFingerId == Input.GetTouch(i).fingerId) {
+                    player.MoveToPosition(touchWorldPosition);
+                    if (Input.GetTouch(i).phase == TouchPhase.Ended || Input.GetTouch(i).phase == TouchPhase.Canceled) {
+                        player.LockedFingerId = null;
+                    }
+                }
+            });
+        }
+    }
+}
--- Assets/Scripts/GameScene/PlayerMovement.cs
+++ Assets/Scripts/GameScene/PlayerMovement.cs
@@ -2,49 +2,43 @@
  
 public class PlayerMovement : MonoBehaviour
 {
-    private const int leftMouseButton = 0;
-    private Vector2 StartPosition => new Vector2(0, -3);
     
-    private bool wasJustClicked = true;
-    private bool canMove;
-    
-    private Rigidbody2D rigidBody;
+    public int? LockedFingerId { get; set; }
     public Transform Bounder;
-    public Collider2D playerCollider;
-    
-    Boundary playerBoundary;
+    public Collider2D PlayerCollider { get; private set; }
+    public PlayerController Controller;
+ 
+    private Rigidbody2D rigidBody;
+    private Boundary playerBoundary;
 
     void Start () {
         rigidBody = GetComponent<Rigidbody2D>();
-        playerCollider = GetComponent<Collider2D>();
+        PlayerCollider = GetComponent<Collider2D>();
         playerBoundary = new Boundary(
             Bounder.GetChild(0).position.y, Bounder.GetChild(1).position.x,
         Bounder.GetChild(2).position.y, Bounder.GetChild(3).position.x
         );
     }
- 
-    void Update () {
-        if (Input.GetMouseButton(leftMouseButton)) {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
- 
-            if (wasJustClicked)
-            {
-                wasJustClicked = false;
 
-                canMove = playerCollider.OverlapPoint(mousePosition);
-            }
-            if (canMove) {
-                var clampedX = Mathf.Clamp(mousePosition.x, playerBoundary.Left, playerBoundary.Right);
-                var clampedY = Mathf.Clamp(mousePosition.y, playerBoundary.Down, playerBoundary.Up);
-                var clampedMousePosition = new Vector2(clampedX, clampedY);
-                rigidBody.MovePosition(clampedMousePosition);
-            }
-        }
-        else {
-            wasJustClicked = true;
-        }
+    private void OnEnable()
+    {
+        Controller.Players.Add(this);
     }
 
+    private void OnDisable()
+    {
+        Controller.Players.Remove(this);
+    }
+    
+    public void MoveToPosition(Vector2 position)
+    {
+        var clampedX = Mathf.Clamp(position.x, playerBoundary.Left, playerBoundary.Right);
+        var clampedY = Mathf.Clamp(position.y, playerBoundary.Down, playerBoundary.Up);
+        var clampedMousePosition = new Vector2(clampedX, clampedY);
+        rigidBody.MovePosition(clampedMousePosition);
+    }
+
+    private Vector2 StartPosition => new Vector2(0, -3);
     public void ResetPosition()
     {
         rigidBody.position = StartPosition;
